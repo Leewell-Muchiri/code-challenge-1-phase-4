2fd626c7251ba84e9607940186137b7a068735b8
Leewell-Muchiri <leewellmacharia10@gmail.com>
2023-03-21 14:54:22 +0300

    debugging

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,25 +1,21 @@
 class RestaurantsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :error_response
-        def index
-            restaurants = Restaurant.all
-            render json: restaurants, include:['restaurant.pizzas'], status: :ok
+    def index
+        restaurants = Restaurant.all
+        render json: restaurants, include:['restaurant.pizzas'], status: :ok
+    end
+    def show
+        restaurants = Restaurant.find(params[:id])
+        render json: restaurants, status:
+    end
+    def destroy
+        restaurants = Restaurant.find(params[:id])
+        restaurants.destroy
+        head :no_content
+    end
     
-        end
-        def show
-            restaurants = Restaurant.find(params[:id])
-             render json: restaurants, status:
-        end
-        def destroy
-            restaurants = Restaurant.find(params[:id])
-            restaurants.destroy
-            head :no_content
-        end
-    
-        private
-        def error_response
-            render json: {error:"Restaurant not found"}, status: :not_found
-        end
-        
-    
-
+    private
+    def error_response
+        render json: {error:"Restaurant not found"}, status: :not_found
+    end
 end
